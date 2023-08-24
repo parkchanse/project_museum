0e9a8b1c95666e9ef6f19516e48dd3e2253e4f26
User -2023XQKFO\Administrator <wj402@naver.com>
2023-08-24 11:37:13 +0900

    (feat)feature_article02 - 박물관 안내 구현
    박물관 안내구현을 완료했습니다.
    
    Resolves: #1

--- js/app.js
+++ js/app.js
@@ -1,30 +1,39 @@
-const toplayout = document.querySelector('.top_layout')
-const bottomlayout = document.querySelector('.bottom_layout')
-const nav = document.querySelector('.nav')
+let slideIndex = 1;
+showSlides(slideIndex)
 
-const top_layout_display = document.querySelector('.top_layout_display')
-const bottom_layout_display = document.querySelector('.bottom_layout_display')
-
-/* function navigatorover() {
-    // nav.classList.add('gnbbg')
-    // bottomlayout.classList.add('opacity')
-    // toplayout.classList.add('gnbbg')
-
-    toplayout.style.display = 'none'
-    bottomlayout.style.opacity = '1' 
-    bottomlayout.style.transition = '1s' 
-
-    
-    
+function plusSlides(e) {
+    showSlides(slideIndex += e)
+    console.log(plusSlides)
 }
-function navigatorleave() {
-    // nav.classList.remove('gnbbg')
-    // bottomlayout.classList.add('opacity')
-    // toplayout.classList.remove('opacity')
-    // bottomlayout.style.opacity = '0' 
 
-}  */
+function currentSlide(e) {
+    showSlides(slideIndex = e)
+    console.log(currentSlide)
+}
+
+function showSlides(e) {
+    let i = 0
+    let slides = document.getElementsByClassName("item")
+    let dots = document.getElementsByClassName("slider_nav_btn")
+
+    if(e > slides.length) {
+        slideIndex = 1
+    }
+    if(e < 1) {
+        slideIndex = slides.length
+    }
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+
+    for(i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    slides[slideIndex-1].style.display = "block"
+    dots[slideIndex-1].className += " active"
+}
 
 
-nav.addEventListener('mouseover', navigatorover)
-nav.addEventListener('mouseleave', navigatorleave)
+
+
+
